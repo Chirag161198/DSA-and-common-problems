7cbdc12940b2244c0b0b9b0966a4b36c41b31b75
Chirag161198 <chirag161198@gmail.com>
2020-05-14 19:30:29 +0530

    Reduced spacecomplexity from O(n) to O(1)

--- Common Problems/longestSubstringWithEqualSum.cpp
+++ Common Problems/longestSubstringWithEqualSum.cpp
@@ -4,30 +4,27 @@
 using namespace std;
 
 int main(){
-    int n, len, maxLength = -1, start, end, halfSum, firstHalfSum, secondHalfSum;
+    int n, len, maxLength = -1, start, end, halfSum, firstHalfSum, totalSum;
     string s;
 
     cin>>s;
     n = s.length();
     
-    float cumulativeSum[n];
-
-    cumulativeSum[0] = int(s[0]-'0');
-    for(int i=1;i<n;i++)
-        cumulativeSum[i] = cumulativeSum[i-1] + int(s[i]-'0');
-
     for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j += 2){
+        
+        firstHalfSum = 0;
+        totalSum = 0;
+
+        for(int j=i+1, k=i;j<n;j += 2, k++){
+            firstHalfSum += int(s[k]-'0');
+            totalSum += int(s[j-1]-'0') + int(s[j]-'0');
+
             len = j-i+1;
             
             if(maxLength>len)
                 continue;
 
-            firstHalfSum = cumulativeSum[i + len/2 - 1];
-            firstHalfSum -= i!=0 ? cumulativeSum[i-1]:0;
-
-            secondHalfSum = cumulativeSum[j]-cumulativeSum[i + len/2 - 1];
-            if(firstHalfSum==secondHalfSum){
+            if(firstHalfSum==totalSum-firstHalfSum){
                 maxLength = len;
                 start = i;
                 end = j;
@@ -36,9 +33,11 @@ int main(){
         }
     }
 
+    cout<<endl;
     if(maxLength==-1)
         cout<<"No such sub-string"<<endl;
     else{
+        cout<<"Sub-string: ";
         for(int i=start;i<=end;i++)
             cout<<s[i];
 
